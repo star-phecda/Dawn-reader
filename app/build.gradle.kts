1cf41308b221fc8807b4699ca75e7f7e59b18a0e
star-phecda <dawnphecda@gmail.com>
2026-09-23 11:27:35 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     
-    id("com.google.devtools.ksp") version "2.0.21-1.0.28"
-    id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.google.devtools.ksp") version "2.3.5" id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
