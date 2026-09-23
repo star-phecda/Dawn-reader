0acbb265e929cd059b5f4770e25268d072bc9523
star-phecda <dawnphecda@gmail.com>
2026-09-23 12:53:52 +0100

    Update Theme.kt

--- app/src/main/java/com/emptycastle/novery/ui/theme/Theme.kt
+++ app/src/main/java/com/emptycastle/novery/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.Shapes
