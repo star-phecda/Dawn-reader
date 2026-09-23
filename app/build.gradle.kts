cfe6c2821071b3f4ec4f96a00c368b19f53f713f
star-phecda <dawnphecda@gmail.com>
2026-09-23 13:01:37 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.emptycastle.novery"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         // Intentionally retained for local data continuity with the original app.
