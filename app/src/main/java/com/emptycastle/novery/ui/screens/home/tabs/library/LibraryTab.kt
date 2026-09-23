2ead99a2f2386b3783a21d5cba457281c272ca5c
star-phecda <dawnphecda@gmail.com>
2026-09-23 14:38:11 +0100

    Update LibraryTab.kt

--- app/src/main/java/com/emptycastle/novery/ui/screens/home/tabs/library/LibraryTab.kt
+++ app/src/main/java/com/emptycastle/novery/ui/screens/home/tabs/library/LibraryTab.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
